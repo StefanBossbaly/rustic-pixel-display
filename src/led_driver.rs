ef6045eb62f1865078dd58f7786e463a99f1a2cd
Stefan Bossbaly <sbossb@gmail.com>
2023-05-05 23:06:32 -0400

    First attempt at threading
    First attempt at having a background thread running the matrix update
    logic and the HTTP server communicating with it.

---
+++ src/led_driver.rs
@@ -0,0 +1,75 @@
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyle},
+    pixelcolor::Rgb888,
+    prelude::*,
+    text::{Alignment, Text},
+    Drawable,
+};
+use rpi_led_panel::{RGBMatrix, RGBMatrixConfig};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+// This will be the shared matrix config between the LedDriver thread and other signaling thread(s)
+// Arc allows it to be shared between threads in a thread-safe manner, Mutex allows the inner data type
+// to be thread safe. Option means that there is no pending configuration update, and Some means that there
+// is a pending configuration update.
+type SharedMatrixConfig = Arc<Mutex<Option<RGBMatrixConfig>>>;
+
+pub struct LedDriver {
+    handle: Option<thread::JoinHandle<()>>,
+    alive: Arc<AtomicBool>,
+    pending_matrix_config: SharedMatrixConfig,
+}
+
+impl LedDriver {
+    pub fn new() -> Self {
+        LedDriver {
+            handle: None,
+            alive: Arc::new(AtomicBool::new(false)),
+            pending_matrix_config: Arc::new(Mutex::new(None)),
+        }
+    }
+
+    pub fn update_config(&mut self, config: RGBMatrixConfig) {
+        let mut pending_config = self.pending_matrix_config.lock().expect("Mutex failed");
+        *pending_config = Some(config);
+    }
+
+    pub fn start(&mut self, config: RGBMatrixConfig) {
+        self.alive.store(true, Ordering::SeqCst);
+
+        let (mut matrix, mut canvas) =
+            RGBMatrix::new(config, 0).expect("Matrix initialization failed");
+
+        // Clone variable that will be moved into the thread
+        let alive = self.alive.clone();
+        let pending_matrix_config = self.pending_matrix_config.clone();
+
+        self.handle = Some(thread::spawn(move || {
+            while alive.load(Ordering::SeqCst) {
+                // Update the configuration, if necessary
+                {
+                    let mut config = pending_matrix_config.lock().expect("Mutex failed");
+                    if let Some(new_config) = config.take() {
+                        println!("Updating config: {:#?}", new_config);
+
+                        (matrix, canvas) =
+                            RGBMatrix::new(new_config, 0).expect("Matrix initialization failed");
+                    }
+                }
+
+                canvas.fill(0, 0, 0);
+
+                let text = Text::with_alignment(
+                    "Hello\nWorld",
+                    Point::new(0, 0),
+                    MonoTextStyle::new(&FONT_6X10, Rgb888::WHITE),
+                    Alignment::Center,
+                );
+                text.draw(canvas.as_mut()).unwrap();
+                canvas = matrix.update_on_vsync(canvas);
+            }
+        }));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 #[macro_use]
 extern crate rocket;
 
+use std::str::FromStr;
+use std::sync::Mutex;
+
+use led_driver::LedDriver;
 use rocket::form::{Context, Contextual, Form, FromForm};
 use rocket::http::Status;
+use rocket::State;
 
 use rocket_dyn_templates::Template;
 
-use tokio::join;
-use tokio::sync::broadcast;
+use rpi_led_panel::{HardwareMapping, LedSequence, RGBMatrixConfig, RowAddressSetterType};
+
+mod led_driver;
 
 #[derive(Debug, FromForm)]
 #[allow(dead_code)]
@@ -36,14 +42,45 @@ fn config() -> Template {
     Template::render("config", &Context::default())
 }
 
+struct DriverState(Mutex<LedDriver>);
+
 // NOTE: We use `Contextual` here because we want to collect all submitted form
 // fields to re-render forms with submitted values on error. If you have no such
 // need, do not use `Contextual`. Use the equivalent of `Form<Submit<'_>>`.
 #[post("/config", data = "<form>")]
-fn submit_config<'r>(form: Form<Contextual<'r, HardwareConfig<'r>>>) -> (Status, Template) {
+fn submit_config<'r>(
+    form: Form<Contextual<'r, HardwareConfig<'r>>>,
+    driver_state: &State<DriverState>,
+) -> (Status, Template) {
     let template = match form.value {
         Some(ref submission) => {
             println!("submission: {:#?}", submission);
+
+            let config = RGBMatrixConfig {
+                hardware_mapping: HardwareMapping::from_str(submission.hardware_mapping)
+                    .expect("Invalid hardware mapping"),
+                rows: submission.rows,
+                cols: submission.cols,
+                refresh_rate: submission.refresh_rate,
+                pi_chip: None,
+                pwm_bits: 6,
+                pwm_lsb_nanoseconds: 130,
+                slowdown: None,
+                interlaced: false,
+                dither_bits: 0,
+                chain_length: submission.chain_length,
+                parallel: submission.parallel,
+                panel_type: None,
+                multiplexing: None,
+                row_setter: RowAddressSetterType::Direct,
+                led_sequence: LedSequence::Rgb,
+            };
+
+            {
+                let mut lock = driver_state.0.lock().expect("lock shared data");
+                lock.update_config(config);
+            }
+
             Template::render("config", &form.context)
         }
         None => Template::render("config", &form.context),
@@ -54,29 +91,34 @@ fn submit_config<'r>(form: Form<Contextual<'r, HardwareConfig<'r>>>) -> (Status,
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (_tx, mut rx) = broadcast::channel::<i8>(10);
-
-    let rocket = rocket::build()
-        .mount("/", routes![config, submit_config])
-        .attach(Template::fairing())
-        .ignite()
-        .await?
-        .launch();
-
-    let background_task = tokio::spawn(async move {
-        tokio::select! {
-            _ = rx.recv() => {
-                println!("Received a message!");
-            }
-            _ = tokio::time::sleep(tokio::time::Duration::from_secs(5)) => {
-                println!("Timed out!");
-            }
-        }
+    let mut led_driver = LedDriver::new();
+    led_driver.start(RGBMatrixConfig {
+        hardware_mapping: HardwareMapping::regular(),
+        rows: 64,
+        cols: 64,
+        refresh_rate: 120,
+        pi_chip: None,
+        pwm_bits: 6,
+        pwm_lsb_nanoseconds: 130,
+        slowdown: None,
+        interlaced: false,
+        dither_bits: 0,
+        chain_length: 1,
+        parallel: 1,
+        panel_type: None,
+        multiplexing: None,
+        row_setter: RowAddressSetterType::Direct,
+        led_sequence: LedSequence::Rgb,
     });
 
-    let (rocket_result, background_result) = join!(rocket, background_task);
-    rocket_result?;
-    background_result?;
+    let _rocket = rocket::build()
+        .mount("/", routes![config, submit_config])
+        .attach(Template::fairing())
+        .manage(DriverState(Mutex::new(led_driver)))
+        .ignite()
+        .await?
+        .launch()
+        .await?;
 
     Ok(())
 }

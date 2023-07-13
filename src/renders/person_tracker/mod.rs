af4a50e23f063cba0ca332fc8a92d61ee52d3fd1
Stefan Bossbaly <sbossb@gmail.com>
2023-07-12 23:05:50 -0400

    Let person tracker track multiple people
    Person tracker can now accept multile people with differnet trackers.

--- src/renders/person_tracker/mod.rs
+++ src/renders/person_tracker/mod.rs
@@ -3,7 +3,7 @@ use anyhow::Result;
 use embedded_graphics::{
     mono_font::{self, MonoTextStyle},
     pixelcolor::Rgb888,
-    prelude::{DrawTarget, Point, RgbColor},
+    prelude::{DrawTarget, Point, RgbColor, Size},
     text::Text,
     Drawable,
 };
@@ -13,7 +13,7 @@ use embedded_layout::{
     View,
 };
 use log::warn;
-use std::convert::Infallible;
+use std::{collections::HashMap, convert::Infallible};
 
 mod home_assistant_tracker;
 mod septa_tracker;
@@ -67,15 +67,15 @@ pub struct PersonTracker<D>
 where
     D: DrawTarget<Color = Rgb888, Error = Infallible>,
 {
-    trackers: Vec<Box<dyn StateProvider<D>>>,
+    person_to_trackers: HashMap<String, Vec<Box<dyn StateProvider<D>>>>,
 }
 
 impl<D> PersonTracker<D>
 where
     D: DrawTarget<Color = Rgb888, Error = Infallible>,
 {
-    pub const fn new(trackers: Vec<Box<dyn StateProvider<D>>>) -> Self {
-        Self { trackers }
+    pub const fn new(person_to_trackers: HashMap<String, Vec<Box<dyn StateProvider<D>>>>) -> Self {
+        Self { person_to_trackers }
     }
 }
 
@@ -84,51 +84,69 @@ where
     D: DrawTarget<Color = Rgb888, Error = Infallible>,
 {
     fn render(&self, canvas: &mut D) -> Result<()> {
-        let render_states = self.trackers.iter().map(|tracker| tracker.provide_state());
+        let mut offset = Point::zero();
+        let canvas_bounds = canvas.bounding_box();
 
-        let mut most_useful_render: Option<Box<dyn State<D>>> = None;
+        for (person_name, trackers) in &self.person_to_trackers {
+            let render_states = trackers.iter().map(|tracker| tracker.provide_state());
 
-        for render_state in render_states {
-            match &most_useful_render {
-                Some(most_useful) => {
-                    if most_useful.usefulness() < render_state.usefulness() {
+            let mut most_useful_render: Option<Box<dyn State<D>>> = None;
+
+            for render_state in render_states {
+                match &most_useful_render {
+                    Some(most_useful) => {
+                        if most_useful.usefulness() < render_state.usefulness() {
+                            most_useful_render = Some(render_state);
+                        }
+                    }
+                    None => {
                         most_useful_render = Some(render_state);
                     }
                 }
-                None => {
-                    most_useful_render = Some(render_state);
+            }
+
+            match most_useful_render {
+                Some(most_useful) => {
+                    let person_layout = LinearLayout::horizontal(Chain::new(Text::new(
+                        person_name,
+                        Point::zero(),
+                        MonoTextStyle::new(&mono_font::ascii::FONT_6X10, Rgb888::WHITE),
+                    )))
+                    .with_alignment(vertical::Center)
+                    .with_spacing(spacing::FixedMargin(6))
+                    .arrange()
+                    .translate(offset);
+
+                    person_layout.draw(canvas).unwrap();
+
+                    let person_size = person_layout.bounds().size();
+
+                    let sub_canvas_offset = Point {
+                        x: offset.x,
+                        y: offset.y + person_size.height as i32,
+                    };
+
+                    let sub_canvas_size = Size {
+                        width: canvas_bounds.size.width,
+                        height: 50,
+                    };
+
+                    most_useful
+                        .sub_render(&mut SubCanvas::new(
+                            sub_canvas_offset,
+                            sub_canvas_size,
+                            canvas,
+                        ))
+                        .unwrap();
+
+                    offset = sub_canvas_offset
+                        + Size {
+                            width: 0,
+                            height: sub_canvas_size.height,
+                        };
                 }
+                None => warn!("No renders"),
             }
-        }
-
-        match most_useful_render {
-            Some(most_useful) => {
-                let person_layout = LinearLayout::horizontal(Chain::new(Text::new(
-                    "Stefan Bossbaly",
-                    Point::zero(),
-                    MonoTextStyle::new(&mono_font::ascii::FONT_6X10, Rgb888::WHITE),
-                )))
-                .with_alignment(vertical::Center)
-                .with_spacing(spacing::FixedMargin(6))
-                .arrange();
-
-                let person_bounds = person_layout.bounds();
-                let canvas_bounds = canvas.bounding_box();
-
-                person_layout.draw(canvas).unwrap();
-
-                let mut sub_canvas = SubCanvas::new(
-                    Point {
-                        x: 0,
-                        y: person_bounds.size.height as i32,
-                    },
-                    canvas_bounds.size - person_bounds.size,
-                    canvas,
-                );
-
-                most_useful.sub_render(&mut sub_canvas).unwrap();
-            }
-            None => warn!("No renders"),
         }
 
         Ok(())

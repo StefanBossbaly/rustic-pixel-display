800b0785cbc599df0dd17cfd49893b826a9c3022
Stefan Bossbaly <sbossb@gmail.com>
2023-07-12 23:11:26 -0400

    Added person tracker to simulator

--- src/bin/simulator.rs
+++ src/bin/simulator.rs
@@ -7,32 +7,78 @@ use embedded_graphics::{
 use embedded_graphics_simulator::{
     OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent, Window,
 };
-use rustic_pixel_display::{render::Render, renders::upcoming_arrivals::UpcomingArrivals};
+use lazy_static::lazy_static;
+use rustic_pixel_display::{
+    render::Render,
+    renders::person_tracker::{
+        HomeAssistantTracker, HomeTrackerConfig, PersonTracker, StateProvider, TransitTracker,
+        TransitTrackerConfig,
+    },
+};
+use std::{collections::HashMap, env::var, vec};
 
 const DISPLAY_SIZE: Size = Size {
-    width: 256,
+    width: 128,
     height: 128,
 };
+
+lazy_static! {
+    static ref HASS_URL: String =
+        var("HASS_URL").expect("Pleases set HASS_URL to the url of the home assistant instance");
+    static ref BEARER_TOKEN: String = var("BEARER_TOKEN")
+        .expect("Please set BEARER_TOKEN environment variable to a long lived access token");
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::init();
 
-    let output_settings = OutputSettingsBuilder::new().scale(4).max_fps(60).build();
+    let output_settings = OutputSettingsBuilder::new().scale(10).max_fps(60).build();
     let mut window = Window::new("Simulator", &output_settings);
     let mut canvas = SimulatorDisplay::<Rgb888>::new(DISPLAY_SIZE);
 
-    let transit_render = Box::new(UpcomingArrivals::new(
-        septa_api::types::RegionalRailStop::SuburbanStation,
-        20,
-    ));
+    let mut person_map: HashMap<String, Vec<Box<dyn StateProvider<_>>>> = HashMap::new();
+
+    person_map.insert(
+        "Stefan".to_owned(),
+        vec![
+            Box::new(TransitTracker::new(TransitTrackerConfig {
+                home_assistant_url: HASS_URL.clone(),
+                home_assistant_bearer_token: BEARER_TOKEN.clone(),
+                person_entity_id: "person.stefan".to_string(),
+            })?),
+            Box::new(HomeAssistantTracker::new(HomeTrackerConfig {
+                home_assistant_url: HASS_URL.clone(),
+                home_assistant_bearer_token: BEARER_TOKEN.clone(),
+                person_entity_id: "person.stefan".to_string(),
+            })?),
+        ],
+    );
+
+    person_map.insert(
+        "Abby".to_owned(),
+        vec![
+            Box::new(TransitTracker::new(TransitTrackerConfig {
+                home_assistant_url: HASS_URL.clone(),
+                home_assistant_bearer_token: BEARER_TOKEN.clone(),
+                person_entity_id: "person.abby".to_string(),
+            })?),
+            Box::new(HomeAssistantTracker::new(HomeTrackerConfig {
+                home_assistant_url: HASS_URL.clone(),
+                home_assistant_bearer_token: BEARER_TOKEN.clone(),
+                person_entity_id: "person.abby".to_string(),
+            })?),
+        ],
+    );
+
+    let person_tracker = PersonTracker::new(person_map);
 
     'render_loop: loop {
         canvas
             .fill_solid(&Rectangle::new(Point::zero(), DISPLAY_SIZE), Rgb888::BLACK)
             .unwrap();
 
-        transit_render.render(&mut canvas).unwrap();
+        person_tracker.render(&mut canvas).unwrap();
         window.update(&canvas);
 
         for event in window.events() {

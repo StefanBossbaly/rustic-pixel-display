a3158f0626622f9524b435bef9b94ce005821c07
Stefan Bossbaly <sbossb@gmail.com>
2023-07-17 22:08:01 -0400

    Continuing to add to weather display

--- src/renders/weather.rs
+++ src/renders/weather.rs
@@ -2,11 +2,15 @@ use anyhow::Result;
 use embedded_graphics::{
     mono_font::{self, MonoTextStyle},
     pixelcolor::Rgb888,
-    prelude::{DrawTarget, Point, RgbColor},
+    prelude::{DrawTarget, Point, RgbColor, WebColors},
     text::Text,
     Drawable,
 };
-use embedded_layout::{layout::linear::LinearLayout, prelude::Chain};
+use embedded_layout::{
+    layout::linear::{spacing, LinearLayout},
+    prelude::Chain,
+    view_group::Views,
+};
 use log::error;
 use parking_lot::Mutex;
 use serde::Deserialize;
@@ -36,13 +40,27 @@ impl From<Location> for weer_api::Query {
 
 #[derive(Debug, Default)]
 struct DisplayForecast {
-    temperature: String,
+    location_name: String,
+    region_name: String,
+    temperature: f32,
+    temperature_str: String,
+    feels_like: f32,
+    feels_like_str: String,
+    wind: String,
+    humidity: String,
 }
 
 impl From<weer_api::Forecast> for DisplayForecast {
     fn from(value: weer_api::Forecast) -> Self {
         Self {
-            temperature: format!("{} °F", value.current.temp_f),
+            location_name: value.location.name.clone(),
+            region_name: value.location.region.clone(),
+            temperature: value.current.temp_f,
+            temperature_str: format!("{} °F", value.current.temp_f),
+            feels_like: value.current.feelslike_f,
+            feels_like_str: format!("{} °F", value.current.feelslike_f),
+            wind: format!("{} mph", value.current.wind_mph),
+            humidity: format!("{} %", value.current.humidity),
         }
     }
 }
@@ -118,11 +136,104 @@ where
     fn render(&self, canvas: &mut D) -> Result<()> {
         let display_state = self.state.lock();
 
-        LinearLayout::vertical(Chain::new(Text::new(
-            display_state.temperature.as_str(),
-            Point::zero(),
-            MonoTextStyle::new(&mono_font::iso_8859_1::FONT_10X20, Rgb888::WHITE),
-        )))
+        let color_from_temp = |temp: f32| -> Rgb888 {
+            if temp > 50.0 && temp <= 70.0 {
+                Rgb888::GREEN
+            } else if temp > 70.0 && temp <= 80.0 {
+                Rgb888::YELLOW
+            } else if temp > 80.0 && temp <= 90.0 {
+                Rgb888::RED
+            } else if temp > 90.0 && temp <= 100.0 {
+                Rgb888::CSS_PURPLE
+            } else if temp > 100.0 {
+                Rgb888::CSS_MAGENTA
+            } else if temp > 40.0 && temp <= 50.0 {
+                Rgb888::YELLOW
+            } else if temp > 30.0 && temp <= 40.0 {
+                Rgb888::RED
+            } else if temp > 20.0 && temp <= 30.0 {
+                Rgb888::CSS_PURPLE
+            } else if temp <= 20.0 {
+                Rgb888::CSS_MAGENTA
+            } else {
+                Rgb888::WHITE
+            }
+        };
+
+        LinearLayout::vertical(
+            Chain::new(Text::new(
+                &display_state.location_name,
+                Point::zero(),
+                MonoTextStyle::new(&mono_font::iso_8859_1::FONT_7X13, Rgb888::WHITE),
+            ))
+            .append(
+                LinearLayout::horizontal(Views::new(&mut [
+                    Text::new(
+                        "Temperature: ",
+                        Point::zero(),
+                        MonoTextStyle::new(&mono_font::iso_8859_1::FONT_6X9, Rgb888::WHITE),
+                    ),
+                    Text::new(
+                        &display_state.temperature_str,
+                        Point::zero(),
+                        MonoTextStyle::new(
+                            &mono_font::iso_8859_1::FONT_6X9,
+                            color_from_temp(display_state.temperature),
+                        ),
+                    ),
+                ]))
+                .arrange(),
+            )
+            .append(
+                LinearLayout::horizontal(Views::new(&mut [
+                    Text::new(
+                        "Feels like: ",
+                        Point::zero(),
+                        MonoTextStyle::new(&mono_font::iso_8859_1::FONT_6X9, Rgb888::WHITE),
+                    ),
+                    Text::new(
+                        &display_state.feels_like_str,
+                        Point::zero(),
+                        MonoTextStyle::new(
+                            &mono_font::iso_8859_1::FONT_6X9,
+                            color_from_temp(display_state.feels_like),
+                        ),
+                    ),
+                ]))
+                .arrange(),
+            )
+            .append(
+                LinearLayout::horizontal(Views::new(&mut [
+                    Text::new(
+                        "Wind: ",
+                        Point::zero(),
+                        MonoTextStyle::new(&mono_font::iso_8859_1::FONT_6X9, Rgb888::WHITE),
+                    ),
+                    Text::new(
+                        &display_state.wind,
+                        Point::zero(),
+                        MonoTextStyle::new(&mono_font::iso_8859_1::FONT_6X9, Rgb888::WHITE),
+                    ),
+                ]))
+                .arrange(),
+            )
+            .append(
+                LinearLayout::horizontal(Views::new(&mut [
+                    Text::new(
+                        "Humidity: ",
+                        Point::zero(),
+                        MonoTextStyle::new(&mono_font::iso_8859_1::FONT_6X9, Rgb888::WHITE),
+                    ),
+                    Text::new(
+                        &display_state.humidity,
+                        Point::zero(),
+                        MonoTextStyle::new(&mono_font::iso_8859_1::FONT_6X9, Rgb888::WHITE),
+                    ),
+                ]))
+                .arrange(),
+            ),
+        )
+        .with_spacing(spacing::FixedMargin(2))
         .arrange()
         .draw(canvas)
         .unwrap();
